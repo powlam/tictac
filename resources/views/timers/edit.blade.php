4fc984c1dff946ccbf99720c01bc4d839ad79172
Paul Albandoz <palbandoz+github@gmail.com>
2026-05-11 10:44:26 +0200

    style: Go back buttons

--- resources/views/timers/edit.blade.php
+++ resources/views/timers/edit.blade.php
@@ -35,8 +35,9 @@
                 </button>
             </form>
 
-            <a href="{{ route('routines.show', $routine) }}" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md shadow-sm text-gray-700 dark:text-neutral-300 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                Go back
+            <a href="{{ route('routines.show', $routine) }}" class="inline-flex place-content-center mt-4 px-4 py-2 border border-gray-300 text-sm font-medium rounded-md shadow-sm text-gray-500 dark:text-neutral-400 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 gap-2">
+                <flux:icon.arrow-left />
+                <span class="place-content-center">Go back</span>
             </a>
         </div>
     </div>

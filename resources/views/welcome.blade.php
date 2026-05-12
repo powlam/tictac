e27e08663392c7141f35440daf60ec4dece80a58
Paul Albandoz <palbandoz+github@gmail.com>
2026-05-12 09:19:22 +0200

    chore: meta version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
 
         <title>{{ __('Welcome') }} - {{ config('app.name', 'Laravel') }}</title>
 
+        <meta name="version" content="{{ config('app.version') }}">
+
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
